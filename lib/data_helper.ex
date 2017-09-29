e0da39ad7873a466c75e5f99836c7cba0e689ea9
Alexandr <alexandr.tutkarev@blockchain.ru>
2017-09-29 15:16:21 +0300

    fix data convert to struct

--- lib/data_helper.ex
+++ lib/data_helper.ex
@@ -2,5 +2,8 @@ defmodule DataHelper do
 
   def struct_from_map([], _opts), do: []
   def struct_from_map([head|tail], as: a_struct), do: [struct_from_map(head, as: a_struct) | struct_from_map(tail, as: a_struct)]
-  def struct_from_map(a_map, as: a_struct), do: struct(a_struct, a_map)
+  def struct_from_map(map, as: a_struct) do
+    attrs = Enum.reduce(map, [], fn {k, v}, acc -> [{String.to_atom(k), v}|acc] end)
+    struct(a_struct, attrs)
+  end
 end

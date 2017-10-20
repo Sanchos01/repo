8e25126b0e16d0ca31d6beb8861779ab7bb49329
Alexandr <alexandr.tutkarev@blockchain.ru>
2017-10-20 19:13:10 +0300

    minor

--- lib/data_helper.ex
+++ lib/data_helper.ex
@@ -1,6 +1,6 @@
 defmodule DataHelper do
 
-  def struct_from_map([], a_struct), do: []
+  def struct_from_map([], _a_struct), do: []
   def struct_from_map([head|tail], a_struct), do: [struct_from_map(head, a_struct) | struct_from_map(tail, a_struct)]
   def struct_from_map(map, a_struct) when is_map(map) do
     attrs = Enum.reduce(map, [], fn {k, v}, acc -> [{String.to_atom(k), v}|acc] end)
